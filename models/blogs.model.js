dd53c46d01affdebdd0fd01441bc05cd63b0c19a
Piyush Kumar <kumarpiyush913565@gmail.com>
2025-07-27 20:51:24 +0530

    add blog model

---
+++ models/blogs.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    title: { type:String, required:true, unique:true },
+    authors: { type: [String]},
+    content: { type:String, default: ""},
+    publishedAt: { type: Date, default: null },
+},{ timestamps: true });
+
+const blogModel = mongoose.model("Blogs",blogSchema,'websiteBlogs');
+
+module.exports = blogModel;
